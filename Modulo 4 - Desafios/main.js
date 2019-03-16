158de5f29872d9de55e5be83c1668efeb19d73c4
jarelio <jareliofilho@gmail.com>
2019-03-16 00:59:27 -0300

    Módulo 4 - Desafio 4

--- Modulo 4 - Desafios/main.js
+++ Modulo 4 - Desafios/main.js
@@ -20,32 +20,36 @@
 //Desafio 2
 
 
-// function adicionar(){
-//     var textLink = document.querySelector("input");
-//     axios.get('https://api.github.com/users/'+textLink.value+'/repos')
-//         .then(function(response){
-//             createList(response.data)
-//         })
-//         .catch(function(error){
-//             console.log(error);
-//             alert("Usuário não existe ou o servidor não respondeu");
-//         });
-// }
+function adicionar(){
+    var ulLink = document.querySelector("ul");
+    ulLink.innerHTML = ' ';
+    var textLink = document.querySelector("input");
+    var liCarregando = document.createElement('li');
+    var textCarregando = document.createTextNode("Carregando...");
+    liCarregando.appendChild(textCarregando);
+    ulLink.appendChild(liCarregando);
+    axios.get('https://api.github.com/users/'+textLink.value+'/repos')
+        .then(function(response){
+            ulLink.removeChild(liCarregando);
+            createList(response.data,ulLink)
+        })
+        .catch(function(error){
+            console.log(error);
+            alert("Usuário não existe ou o servidor não respondeu");
+        });
+}
 
-// function createList(list){
-//     var ulLink = document.querySelector("ul");
-//     ulLink.innerHTML = ' ';
-//     var nomes = list.map(el => el.name);
-//     console.log(nomes);
-//     nomes.forEach(el => {
-//         var repo = document.createElement('li');
-//         var text = document.createTextNode(el);
-//         repo.appendChild(text);
-//         ulLink.appendChild(repo);
-//     });
-// }
+function createList(list,ulLink){
+    var nomes = list.map(el => el.name);
+    console.log(nomes);
+    nomes.forEach(el => {
+        var repo = document.createElement('li');
+        var text = document.createTextNode(el);
+        repo.appendChild(text);
+        ulLink.appendChild(repo);
+    });
+}
 
 
 //Desafio 3
 
-//Desafio 4
