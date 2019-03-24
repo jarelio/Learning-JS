b19e2457a7d2f4f5f332bceddadbeda6bd537ded
jarelio <jareliofilho@gmail.com>
2019-03-24 11:21:22 -0300

    Rest and Spread

--- curso-es6/bundle.js
+++ curso-es6/bundle.js
@@ -1,18 +1,68 @@
 "use strict";
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+// REST
 var usuario = {
-  nome: 'jarelio',
-  idade: '18',
-  endereco: {
-    rua: 'Rua verde 1',
-    cidade: 'Fortaleza'
+  nome: 'Jarelio',
+  idade: 18,
+  empresa: 'UFC'
+};
+
+var nome = usuario.nome,
+    resto = _objectWithoutProperties(usuario, ["nome"]);
+
+console.log(nome, resto); // REST in vectors
+
+var arr = [1, 2, 3, 4];
+var a = arr[0],
+    b = arr[1],
+    c = arr.slice(2);
+console.log(a, b, c); // REST in functions parameters
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
   }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+}
+
+;
+console.log(soma(1, 3, 4));
+
+var soma = function soma() {
+  for (var _len2 = arguments.length, params = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+    params[_key2] = arguments[_key2];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 };
 
-var retornaNome = function retornaNome(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  return console.log(nome, idade);
+console.log(soma(1, 3, 4)); // SPREAD
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
+var usuario1 = {
+  nome: 'Jarelio',
+  idade: 18,
+  empresa: 'UFC'
 };
 
-retornaNome(usuario);
+var usuario2 = _objectSpread({}, usuario1, {
+  nome: 'Joao'
+});
+
+console.log(usuario2);
--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,12 +1,44 @@
+// REST
+
 const usuario = {
-    nome: 'jarelio',
-    idade: '18',
-    endereco: {
-        rua: 'Rua verde 1',
-        cidade: 'Fortaleza'
-    },
+    nome: 'Jarelio',
+    idade: 18,
+    empresa: 'UFC'
 };
 
-const retornaNome = ({nome, idade}) => console.log(nome,idade);
+const {nome, ...resto} = usuario;
 
-retornaNome(usuario);
+console.log(nome, resto);
+
+// REST in vectors
+
+const arr = [1,2,3,4];
+
+const [a, b, ...c] = arr;
+
+console.log(a,b,c);
+
+// REST in functions parameters
+function soma (...params){
+    return params.reduce((total,next) => total+next); 
+};
+console.log(soma(1,3,4));
+
+const soma = (...params) => params.reduce((total,next) => total+next);
+console.log(soma(1,3,4));
+
+// SPREAD
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+const usuario1 = {
+    nome: 'Jarelio',
+    idade: 18,
+    empresa: 'UFC'
+}
+
+const usuario2 = {...usuario1, nome: 'Joao'};
+console.log(usuario2);
