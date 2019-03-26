b5f9e7ce1202bdd78dd55372d8fbb003368ec479
jarelio <jareliofilho@gmail.com>
2019-03-25 23:23:18 -0300

    Exercício 1 - Módulo 1

---
+++ curso-es6/exercicios-modulo1/bundle.js
@@ -0,0 +1,30 @@
+// 1º Exercicio
+
+/* class Usuario{
+    constructor(email,senha){
+        this.email = email;
+        this.senha = senha;
+    }
+    isAdmin(){
+        return this.admin === true;
+    }
+
+}
+
+class Admin extends Usuario{
+    constructor(email,senha){
+        super(email,senha);
+        this.admin = true;
+    }
+
+}
+
+const novoUser = new Usuario("jarelio", "123");
+const novoAdmin = new Admin("jarelio", "123");
+
+console.log(novoUser.isAdmin());
+console.log(novoUser.admin); //undefined
+console.log(novoAdmin.isAdmin());
+console.log(novoAdmin.admin); //true */
+// 2º Exercício
+"use strict";
